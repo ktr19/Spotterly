9d92e03eb5721701901ac9b2a229def2c49fb27c
Aleaand <alejandraandrade1224@gmail.com>
2025-01-18 19:03:12 +0100

    Suscripción terminada

--- app/src/main/java/com/example/spotterly/ui/suscripciones/SuscripcionesFragment.java
+++ app/src/main/java/com/example/spotterly/ui/suscripciones/SuscripcionesFragment.java
@@ -1,63 +1,144 @@
 package com.example.spotterly.ui.suscripciones;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.spotterly.databinding.FragmentSuscripcionesBinding;
+import com.example.spotterly.R;
 
 public class SuscripcionesFragment extends Fragment {
 
-    private FragmentSuscripcionesBinding binding;
+    private FrameLayout frameMensual, frameTrimestral, frameSemestral, frameAnual;
+    private Button btCancelarSuscripcion;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentSuscripcionesBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_suscripciones, container, false);
+        btCancelarSuscripcion = rootView.findViewById(R.id.btCancelarSuscripcion);
+        frameMensual = rootView.findViewById(R.id.frameMensual);
+        frameTrimestral = rootView.findViewById(R.id.frameTrimestral);
+        frameSemestral = rootView.findViewById(R.id.frameSemestral);
+        frameAnual = rootView.findViewById(R.id.frameAnual);
+        btCancelarSuscripcion.setVisibility(View.GONE);
+        configurarEventos();
+        return rootView;
+    }
 
-        // Obtener la referencia al botón de cancelar suscripción
-        Button btCancelarSuscripcion = binding.btCancelarSuscripcion;
-
-        // Establecer el OnClickListener para el botón
-        btCancelarSuscripcion.setOnClickListener(new View.OnClickListener() {
+    private void configurarEventos() {
+        frameMensual.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mostrarAlerta();
+                mostrarAlertaCompra("Mensual");
             }
         });
 
-        return root;
+        frameTrimestral.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarAlertaCompra("Trimestral");
+            }
+        });
+
+        frameSemestral.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarAlertaCompra("Semestral");
+            }
+        });
+
+        frameAnual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarAlertaCompra("Anual");
+            }
+        });
+
+        btCancelarSuscripcion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarAlertaCancelar();
+            }
+        });
     }
 
-    private void mostrarAlerta() {
-        // Crear el AlertDialog
+    private void mostrarAlertaCancelar() {
+        // Crear el AlertDialog para cancelar la suscripción
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setMessage("¿Estás seguro de que deseas cancelar la suscripción?")
-                .setCancelable(false)  // El diálogo no se puede cancelar tocando fuera
-                .setPositiveButton("Sí", (dialog, id) -> {
-                    // Acción si el usuario presiona "Sí"
-                    Toast.makeText(getActivity(), "Suscripción cancelada", Toast.LENGTH_SHORT).show();
+                .setCancelable(false)
+                .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(getActivity(), "Suscripción cancelada", Toast.LENGTH_SHORT).show();
+                        mostrarSuscripciones(); // Volver a mostrar las opciones de suscripción
+                    }
                 })
-                .setNegativeButton("No", (dialog, id) -> {
-                    // Acción si el usuario presiona "No"
-                    dialog.dismiss();  // Simplemente cierra el diálogo
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(getActivity(), "No se canceló la suscripción", Toast.LENGTH_SHORT).show();
+                        ocultarSuscripciones();
+                    }
                 });
-
-        // Mostrar el diálogo
         AlertDialog alert = builder.create();
         alert.show();
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    private void mostrarAlertaCompra(String tipo) {
+        // Crear el AlertDialog para confirmar la compra de la suscripción
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("¿Quieres comprar la opción " + tipo + "?")
+                .setCancelable(false)
+                .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Acción al confirmar la compra
+                        Toast.makeText(getActivity(), "Compra de suscripción " + tipo + " confirmada", Toast.LENGTH_SHORT).show();
+                        procesarSuscripcion(tipo);
+                        ocultarSuscripciones();
+                        btCancelarSuscripcion.setVisibility(View.VISIBLE);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Acción al cancelar la compra
+                        Toast.makeText(getActivity(), "Lo tienes al alcance de tu mano", Toast.LENGTH_SHORT).show();
+                        mostrarSuscripciones();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void procesarSuscripcion(String tipo) {
+        ///////////////////////// Lógica para procesar la suscripción seleccionada
+        Toast.makeText(getActivity(), "Procesando suscripción: " + tipo, Toast.LENGTH_SHORT).show();
+    }
+
+    private void ocultarSuscripciones() {
+        frameMensual.setVisibility(View.GONE);
+        frameTrimestral.setVisibility(View.GONE);
+        frameSemestral.setVisibility(View.GONE);
+        frameAnual.setVisibility(View.GONE);
+    }
+
+    private void mostrarSuscripciones() {
+        frameMensual.setVisibility(View.VISIBLE);
+        frameTrimestral.setVisibility(View.VISIBLE);
+        frameSemestral.setVisibility(View.VISIBLE);
+        frameAnual.setVisibility(View.VISIBLE);
+        btCancelarSuscripcion.setVisibility(View.GONE);
     }
 }
