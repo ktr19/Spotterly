b823f6740f7fd690bfc6f84b53d19dbd0255d2a5
Aleaand <alejandraandrade1224@gmail.com>
2025-01-18 20:32:51 +0100

    Vistas aparcamiento terminadas

--- app/src/main/java/com/example/spotterly/ui/consultar/ConsultarFragment.java
+++ app/src/main/java/com/example/spotterly/ui/consultar/ConsultarFragment.java
@@ -1,14 +1,18 @@
 package com.example.spotterly.ui.consultar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.SeekBar;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.example.spotterly.R;
 import com.example.spotterly.databinding.FragmentConsultarBinding;
 
 public class ConsultarFragment extends Fragment {
@@ -19,9 +23,22 @@ public class ConsultarFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentConsultarBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        //Barra
+        SeekBar seekBar = binding.seekBar;
+        seekBar.setProgress(5);
+        seekBar.setEnabled(false);
+
+        //Botón que redirecciona
+        Button btRedirigir = binding.btverGuardarParking;
+        btRedirigir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_main);
+                navController.navigate(R.id.nav_localizacion);
+            }
+        });
         return root;
     }
-
     @Override
     public void onDestroyView() {
         super.onDestroyView();
